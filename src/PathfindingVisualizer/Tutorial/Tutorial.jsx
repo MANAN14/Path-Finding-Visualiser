691a87635ee067f2e1e850771935922a80b74ea1
MANAN14 <56201740+MANAN14@users.noreply.github.com>
2022-08-03 18:19:13 +0530

    Update Tutorial.jsx

--- src/PathfindingVisualizer/Tutorial/Tutorial.jsx
+++ src/PathfindingVisualizer/Tutorial/Tutorial.jsx
@@ -5,67 +5,7 @@ import end from '../Node/images/end.png';
 import weight from '../Node/images/weight.png';
 import visitedWeight from '../Node/images/visitedWeight.png';
 import pathWeight from '../Node/images/pathWeight.png';
-
 import "./Tutorial.css";
-function Tutorial() 
-{
-
-  useEffect(()=>{
-     if (getCookie('visited') == null){
-      showPopUp();
-      setCookie('visited', true);
-    }
-    else {
-      skipPopUp();
-    }
-  },[])
-    return (
-      <div className="PopUpTextBox">
-        <header>
-          <p>Welcome to Dijkstra Search Visualizer</p>
-          <h3 onClick={skipPopUp}>x</h3>
-        </header>
-
-        <div className="PopUpIntroductionText">
-          <h3>Quick Walkthrough to Visualizer</h3>
-          <div className="player">
-            
-            <label htmlFor="start">Start : </label>
-            <img src={start} name="start" alt=""/>
-
-            <label htmlFor="end">End : </label>
-            <img src={end} name="end" alt=""/>
-
-            <label htmlFor="wall">Wall : </label>
-            <div style={{background:"black"}} name="wall"></div>
-
-            <label htmlFor="wall">Visited Node : </label>
-            <div style={{backgroundColor: 'rgba(0, 190, 218, 0.75)'}} name="wall"></div>
-
-            <label htmlFor="weight">Weight : </label>
-            <img src={weight} name="weight" alt=""/>
-
-            <label htmlFor="visitedWeight">Visited Weight : </label>
-            <img src={visitedWeight} name="visitedWeight" alt=""/>
-
-            <label htmlFor="pathWeight">Weight on Path : </label>
-            <img src={pathWeight} name="pathWeight" alt=""/>
-
-          </div>
-          <div>
-
-          </div>
-        </div>
-
-        <div className="skipBox">
-          <small>*You can view this anytime by clicking on <span>[Search Visualizer]</span></small>
-          <button onClick={skipPopUp}>Done !</button>
-        </div>
-
-        
-      </div>
-    )
-}
 
 const skipPopUp = () => {
   const elem = document.querySelector(".PopUp");
@@ -94,4 +34,60 @@ function getCookie(name) {
   }
   return null;
 }
+function Tutorial() 
+{
+
+  useEffect(()=>{
+     if (getCookie('visited') == null){
+      showPopUp();
+      setCookie('visited', true);
+    }
+    else {
+      skipPopUp();
+    }
+  },[])
+    return (
+      <div className="PopUpTextBox">
+        <header>
+          <p>Welcome to Path Finding Visualizer</p>
+          <h3 onClick={skipPopUp}>x</h3>
+        </header>
+
+        <div className="PopUpIntroductionText">
+          <h3>Quick Walkthrough</h3>
+          <div className="player">
+            
+            <label htmlFor="start">Start : </label>
+            <img src={start} name="start" alt=""/>
+
+            <label htmlFor="end">End : </label>
+            <img src={end} name="end" alt=""/>
+
+            <label htmlFor="wall">Wall : </label>
+            <div style={{background:"black"}} name="wall"></div>
+
+            <label htmlFor="wall">Visited Node : </label>
+            <div style={{backgroundColor: 'rgba(0, 190, 218, 0.75)'}} name="wall"></div>
+
+            <label htmlFor="weight">Weight : </label>
+            <img src={weight} name="weight" alt=""/>
+
+            <label htmlFor="visitedWeight">Visited Weight : </label>
+            <img src={visitedWeight} name="visitedWeight" alt=""/>
+
+            <label htmlFor="pathWeight">Weight on Path : </label>
+            <img src={pathWeight} name="pathWeight" alt=""/>
+
+          </div>
+          <div>
+          </div>
+        </div>
+        <div className="skipBox">
+          <small>*You can view this anytime by clicking on <span>[Path Finding Visualizer]</span></small>
+          <button onClick={skipPopUp}>Done !</button>
+        </div> 
+      </div>
+    )
+}
+
 export {skipPopUp,showPopUp,Tutorial};
